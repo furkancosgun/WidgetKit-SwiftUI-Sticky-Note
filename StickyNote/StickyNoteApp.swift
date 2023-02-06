8224fb077ab757250b6772c9b284f69436dcd78d
Furkan COSGUN <furkan51cosgun@gmail.com>
2023-02-06 20:57:52 +0300

    BASE

---
+++ StickyNote/StickyNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickyNoteApp.swift
+//  StickyNote
+//
+//  Created by Furkan on 5.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct StickyNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StickyNoteWidget/StickyNoteWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  StickyNoteWidgetBundle.swift
+//  StickyNoteWidget
+//
+//  Created by Furkan on 5.02.2023.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct StickyNoteWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        StickyNoteWidget()
+    }
+}
